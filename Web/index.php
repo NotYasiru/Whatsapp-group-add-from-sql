27dcb234274a94c4c59838c4cdeb5d825321fbfe
Yasiru <92869313+Yasiru100@users.noreply.github.com>
2022-04-11 20:24:53 +0530

    number submit form html csss

---
+++ Web/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Whatsapp Group Add</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form action="" method="POST">
+        <h3>
+            <span>Name : </span>
+            <input type="text" name="uname">
+        </h3>
+        <h3>
+            <span>Phone Number : </span>
+            <input type="text" name="phoneno">
+        </h3>
+    </form>
+</body>
+</html>
